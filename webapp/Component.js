9dc08f7ea375a0bc35eccec6734818a527d18776
Miguel de Lucio Rodríguez <m.de.lucio.rodriguez@accenture.com>
2022-01-14 12:32:41 +0100

    language

--- webapp/Component.js
+++ webapp/Component.js
@@ -25,6 +25,9 @@ sap.ui.define([
 
                 // set the device model
                 this.setModel(models.createDeviceModel(), "device");
+
+                // set language
+                sap.ui.getCore().getConfiguration().setLanguage("es_ES");
             }
         });
     }
